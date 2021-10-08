61997729229c541903489e938b3b762da10fbe81
ChillFish8 <Harrison.Burt@softwire.com>
2021-10-08 11:44:32 +0100

    add the logic for red-3

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -6,42 +6,78 @@ namespace Chessington.GameEngine.Pieces
 {
     public class Pawn : Piece
     {
-        private bool hasMoved = false;
+        private bool _hasMoved;
+        private readonly int _modifier = 1;
+        private Square _currentLocation;
+        private Board _board;
+        private bool _spaceClear = true;
 
-        public Pawn(Player player) 
-            : base(player) { }
+        private readonly List<Action> _pipeline;
+        public Pawn(Player player)
+            : base(player)
+        {
+            if (Player == Player.White)
+            {
+                _modifier = -1;
+            }
+
+            _pipeline = new List<Action>
+            {
+                CheckMoved,
+            };
+        }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            var modifier = 1;
-            if (Player == Player.White)
-            {
-                modifier = -1;
-            }
+            _board = board;
+            _currentLocation = board.FindPiece(this);
+            InvokePipeline();
             
-            var currentLocation = board.FindPiece(this);
-            checkMoved(currentLocation);
+            if (!SpaceClear())
+            {
+                return new List<Square>();
+            }
             
             var moves = new List<Square>
             {
-                Square.At(currentLocation.Row + modifier, currentLocation.Col)
+                GetNextSquare()
             };
 
-            if (hasMoved) return moves;
-            modifier *= 2;
-            moves.Add(Square.At(currentLocation.Row + modifier, currentLocation.Col));
+            if (_hasMoved) return moves;
+            
+            if (SpaceClear(2))
+                moves.Add(GetNextSquare(2));
 
             return moves;
         }
 
-        private void checkMoved(Square location)
+        private void InvokePipeline()
         {
-            if (!hasMoved & Player == Player.White & location.Row != 7)
+            foreach (var action in _pipeline)
             {
-                hasMoved = true;
-            } else if (!hasMoved & Player == Player.Black & location.Row != 1)
+                action();
+            }
+        }
+
+        private Square GetNextSquare(int boost = 1)
+        {
+            return Square.At(_currentLocation.Row + (_modifier * boost), _currentLocation.Col);
+        }
+
+        private bool SpaceClear(int lookAhead = 1)
+        {
+            var next = GetNextSquare(lookAhead);
+            return _board.GetPiece(next) is null;
+        }
+
+        private void CheckMoved()
+        {
+            if (!_hasMoved & Player == Player.White & _currentLocation.Row != 7)
             {
-                hasMoved = true;
+                _hasMoved = true;
+            } else if (!_hasMoved & Player == Player.Black & _currentLocation.Row != 1)
+            {
+                _hasMoved = true;
             }
         }
     }
